b7518aa10b641c9789a4998073798b41ce8f9b97
TizianoAugustoCastroPerez <49189896@est.ort.edu.ar>
2026-09-10 10:15:51 -0300

    Cambiar pines I2C por diferencias en las tensiones
    Updated I2C SDA and SCL pin definitions and initialization.

--- programas/LCD/ProgramaLCD/ProgramaLCD.ino
+++ programas/LCD/ProgramaLCD/ProgramaLCD.ino
@@ -2,13 +2,13 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-#define SDA 21
-#define SCL 22
+#define SDA 18
+#define SCL 19
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
   // Inicialización explícita de I2C en pines 21 (SDA) y 22 (SCL)
-  Wire.begin(SDA, SCL);
+  Wire1.begin(SDA, SCL);
   lcd.init();          // Inicializa el LCD
   lcd.backlight();     // Enciende la luz de fondo
   lcd.setCursor(0, 0); // Columna 0, Fila 0
